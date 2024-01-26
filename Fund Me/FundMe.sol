56b5607d2a8cab8d13d72cfd244654decdaa00ac
FaezehShakouri <f.shakori.com@gmail.com>
2024-01-26 20:44:41 +0330

    Create fundMe contract | Get price of eth in usd from chainlink

---
+++ Fund Me/FundMe.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.12;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract FundMe {
+
+    uint256 minimumValueInUsd = 50 * 1e18;
+
+
+    function fund() public payable {
+        require(getConvertedValue(msg.value) >= minimumValueInUsd, "Did not send enough");
+    }
+
+    function getConvertedValue(uint256 _value) public view returns (uint256) {
+        return (getPrice() * _value) / 1e18;
+    }
+
+    function getPrice() public view returns (uint256) {
+        AggregatorV3Interface dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        (,int256 answer,,,) = dataFeed.latestRoundData();
+        return uint256(answer * 1e10);
+    }
+
+    function getVersion() public view returns (uint256) {
+        AggregatorV3Interface dataFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        return dataFeed.version();
+    }
+   
+}
